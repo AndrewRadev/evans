64829cb32bf6bfe46f2aba4eff2079ec1919307a
Andrew Radev <andrey.radev@gmail.com>
2020-10-08 15:25:09 +0300

    Render video links

--- app/helpers/lectures_helper.rb
+++ app/helpers/lectures_helper.rb
@@ -10,4 +10,12 @@ module LecturesHelper
       lecture[:url]
     end
   end
+
+  def lecture_video_links(lecture)
+    video_links = (lecture[:videos] || []).map.with_index do |url, video_index|
+      link_to "Видео #{video_index + 1}", url, target: '_blank'
+    end
+
+    video_links.join(', ').html_safe
+  end
 end
