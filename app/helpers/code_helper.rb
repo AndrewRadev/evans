85255ff3a4d41ff9ed041ed55f9de407eab0d8cd
Andrew Radev <andrey.radev@gmail.com>
2022-06-19 10:14:41 +0300

    Inline function used in only one place

--- app/helpers/code_helper.rb
+++ app/helpers/code_helper.rb
@@ -5,15 +5,13 @@ module CodeHelper
     render '_formatted_code/code', code: formatted_code, copyable_code: code_text, commentable: false
   end
 
-  def code_with_comments(formatted_code, commentable, form_partial = nil, copyable_code: nil, **form_partial_args)
-    render '_formatted_code/code', code: formatted_code, copyable_code: copyable_code, commentable: commentable,
-                                   form_partial: form_partial, form_partial_args: form_partial_args
-  end
-
   def solution_code(formatted_code, revision, commentable, copyable_code: nil)
-    code_with_comments formatted_code, commentable, 'solutions/inline_comment_form',
-                       copyable_code: copyable_code,
-                       revision: revision
+    render '_formatted_code/code',
+      code: formatted_code,
+      copyable_code: copyable_code,
+      commentable: commentable,
+      form_partial: 'solutions/inline_comment_form',
+      form_partial_args: { revision: revision }
   end
 
   def inline_comment(comment)
